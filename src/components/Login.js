0ac56fd876f4be17cb68c03138460cad4bdc45d2
tedogls333 <tedogls3@gmail.com>
2024-10-21 16:10:21 +0200

    validating input

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,13 @@ export default function Login() {
     password: "",
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
+
   function handleSubmit(e) {
     e.preventDefault();
 
@@ -22,6 +29,18 @@ export default function Login() {
       ...prevValues,
       [identifier]: value,
     }));
+
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
+    }));
   }
 
   return (
@@ -35,9 +54,13 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur("email")}
             value={enteredValues.email}
             onChange={(e) => handleInputChange("email", e.target.value)}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email addres.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
