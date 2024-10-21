2db78be4524fa27f417dc9f6d570171554b82961
tedogls333 <tedogls3@gmail.com>
2024-10-21 13:06:06 +0200

    add new states

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,51 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(
+      "User email: " + enteredEmail,
+      "User password: " + enteredPassword
+    );
+  }
+
+  function handleEmailChange(e) {
+    setEnteredEmail(e.target.value);
+  }
+
+  function handlePasswordChange(e) {
+    setEnteredPassword(e.target.value);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={enteredEmail}
+            onChange={handleEmailChange}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={enteredPassword}
+            onChange={handlePasswordChange}
+          />
         </div>
       </div>
 
