189e13d0e97680a9a9d93c5df79e40c40eeae993
tedogls333 <tedogls3@gmail.com>
2024-10-21 12:46:23 +0200

    add markup for forms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Header from "./components/Header";
+import Login from "./components/Login";
+
 function App() {
-  return <div>hello tako</div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Login />
+      </main>
+    </>
+  );
 }
 
 export default App;
