ba68f7ee2ce61a3bbcf1e066559bdab3d5709db5
tedogls333 <tedogls3@gmail.com>
2024-10-21 14:58:47 +0200

    forms-signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
-import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        <Signup />
       </main>
     </>
   );
