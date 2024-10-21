6d3c824feda3a3f8d8db2ba73ed82a558e14a0ee
tedogls333 <tedogls3@gmail.com>
2024-10-21 15:19:12 +0200

    add new formData

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,15 @@
 export default function Signup() {
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const fd = new FormData(e.target);
+    const acquisitionChannel = fd.getAll("acquisition");
+    const data = Object.fromEntries(fd.entries());
+    data.acquisition = acquisitionChannel;
+    console.log(data);
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
